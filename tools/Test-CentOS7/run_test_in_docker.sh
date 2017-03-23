513f18a183ae8f8d27184eb5b5e8923f421e9490
Hendrik S <hendrik.siewert1995@gmail.com>
2017-03-23 15:36:17 +0100

    Docker doesn't start if you dont have the binary

--- tools/Test-CentOS7/run_test_in_docker.sh
+++ tools/Test-CentOS7/run_test_in_docker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-cp ../../pgglaskugel .
+if [ -f ../../pgglaskugel ]
+  then
+    cp ../../pgglaskugel .
+  else
+    echo "ERROR: Could not find pgglaskugel"
+    exit 1
+fi
+
+echo "GPG takes longer when generating keys if you don't have enough entropy"
 sudo docker build -t=pgglaskugelcentos7 .
 sudo docker run -it pgglaskugelcentos7
 rm pgglaskugel
