92a05a90ef0cb97028b18497b22d7ce9da5e4d9f
Alexander Sosna <alexander.sosna@credativ.de>
2017-03-23 20:59:43 +0100

    Test the testsuite

--- util/general.go
+++ util/general.go
@@ -20,7 +20,6 @@
 package util
 
 import (
-	"bufio"
 	"errors"
 	"fmt"
 	"io"
@@ -66,15 +65,7 @@ func MustAnswerConfirmation(msg string) (confirmed bool) {
 }
 
 func WatchOutput(input io.Reader, outputFunc func(args ...interface{})) {
-	log.Debug("watchOutput started")
-	scanner := bufio.NewScanner(input)
-	for scanner.Scan() {
-		outputFunc(scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		outputFunc("error reading input:", err)
-	}
-	log.Debug("watchOutput end")
+	log.Debug("nop")
 }
 
 // Exists returns whether the given file or directory exists or not
