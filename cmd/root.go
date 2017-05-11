038659dfff2625c798b21df1f73ae4599e2dea52
Danilo Endesfelder <danilo.endesfelder@credativ.de>
2017-05-11 13:11:23 +0200

    final fix for whitelist

--- cmd/root.go
+++ cmd/root.go
@@ -148,7 +148,7 @@ func checkContainswhitelist(command string) bool {
 // Execute adds all child commands to the root command sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	if checkContainswhitelist(os.Args[0]) {
+	if checkContainswhitelist(os.Args[1]) {
 		if err := RootCmd.Execute(); err != nil {
 			fmt.Println(err)
 			os.Exit(-1)
