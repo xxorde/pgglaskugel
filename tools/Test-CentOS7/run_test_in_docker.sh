959a5707b566d20f6383632559d30b73da4e6502
sosna <alexander@xxor.de>
2017-05-17 17:08:17 +0200

    Fix tests

--- tools/Test-CentOS7/run_test_in_docker.sh
+++ tools/Test-CentOS7/run_test_in_docker.sh
@@ -12,7 +12,4 @@ fi
 echo "GPG takes longer when generating keys if you don't have enough entropy"
 sudo docker build -t=pgglaskugelcentos7 .
 
-sudo docker run -it pgglaskugelcentos7 /usr/bin/pgCentOS7.sh file
-sudo docker run -it pgglaskugelcentos7 /usr/bin/pgCentOS7.sh s3
-sudo docker run -it pgglaskugelcentos7 /usr/bin/pgCentOS7.sh minio
-sudo docker run -it pgglaskugelcentos7 /usr/bin/pgCentOS7.sh minioC
+sudo docker run -it pgglaskugelcentos7 /usr/bin/pgCentOS7.sh $1
